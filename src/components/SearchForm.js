34552e1c33c98fb1c883c4599acd18dfb097e279
27medMo7sen <am6945@fayoum.edu.eg>
2024-09-23 18:32:25 +0300

    Refactor CSS styles for MainNavigation and SearchForm components

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,11 +4,20 @@ import classes from "./SearchForm.module.css";
 export const SearchForm = () => {
   const isDarkMode = useSelector((state) => state.ui.darkMode);
   return (
-    <form className={classes.SearchForm}>
-      <input className={classes.Input} type="text" placeholder="Search..." />
-      <button className={classes.Button} type="submit">
-        {isDarkMode ? <DarkModeSearchIcon /> : <LightModeSearchIcon />}
-      </button>
+    <form>
+      <div className={classes["Search-container"]}>
+        <input className={classes.Input} type="text" placeholder="Search..." />
+        <button className={classes.Button} type="submit">
+          {isDarkMode ? <DarkModeSearchIcon /> : <LightModeSearchIcon />}
+        </button>
+      </div>
+      {/* <div className={classes["Search-results"]}>
+        <div>ahmed</div>
+        <div>ahmed</div>
+        <div>ahmed</div>
+        <div>ahmed</div>
+        <div>ahmed</div>
+      </div> */}
     </form>
   );
 };
